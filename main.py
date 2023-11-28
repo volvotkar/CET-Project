51b935fe5e8a4f9ba369e0c5ae15daadf51898c6
probablyliquid <111677910+probablyliquid@users.noreply.github.com>
2023-11-28 12:25:18 +0530

    initial

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+Variables:
+1. Room No.
+2. Sunlight direction
+3. Sunlight intensity (based on time)
+4. Time of Day
+
+Constants:
+1. Number of furniture
+2. Number of heat sources excluding Sun (projector, server box, etc)
+(try to make it a dictionary)
+"""
+
+room = {"chairs": 97}
